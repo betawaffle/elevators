2a447e2d39c147df8a2ffbe19cb6254248aa2a7f
Andrew Hodges <betawaffle@gmail.com>
2015-08-01 10:23:27 -0400

    More WIP (to show progress)
    I'm running out of time, so I'm starting to document my idea since I probably won't be able to write all the code in time.

--- README.md
+++ README.md
@@ -13,3 +13,23 @@ go get github.com/betawaffle/elevators
 ```
 elevators -logtostderr -stderrthreshold INFO
 ```
+
+## Algorithm
+
+### Deciding which floors to stop at
+
+When someone presses a button in the elevator, the pressed floor number should
+be added to a sorted set of target floor numbers. Each car has an associated
+momentum. The car should stop at each floor in target set of floors until there
+are no more in the direction of momentum. When the number of target floors in
+the direction of momentum is empty, the direction of momentum should be
+re-evaluated to either: stop if there are no target floors remaining in any
+direction; reverse direction if there are floors remaining.
+
+With this system, passengers can press buttons for intermediate floors, and the
+elevator will stop at them on the way to the ultimate floor. Non-intermediate
+floors will be arrived at after the ultimate floor has been reached.
+
+### Deciding which elevator to call
+
+TODO
--- elevator/car.go
+++ elevator/car.go
@@ -16,6 +16,12 @@ type Car struct {
 	// Direction is the direction the elevator was moving as of LastPing.
 	Direction Direction
 
+	// Momentum is used to keep the elevator moving is the same direction to
+	// preserve fairness between the occupants. The elevator should keep moving in
+	// the same direction until it reaches the highest or lowest requested floor,
+	// before going in the other direction to fulfill remaining requests.
+	Momentum Direction
+
 	// Aligned indicates whether the elevator was aligned with a floor, ready for
 	// the doors to open, as of LastPing.
 	Aligned bool
@@ -28,4 +34,40 @@ type Car struct {
 }
 
 func (car *Car) step() {
+	// Update the floor based on the current movement.
+	car.Floor = car.Floor + int(car.Direction)
+
+	if !car.shouldStop() {
+		return
+	}
+
+	if car.Direction != Stopped {
+		// stop the car, but preserve the momentum
+		car.Momentum, car.Direction = car.Direction, Stopped
+		return // delay alignment by one tick
+	}
+	if !car.Aligned {
+		car.Aligned = true
+		return // delay opening the doors by one tick
+	}
+
+	// decide if we should stop, keep going, or change direction
+	car.checkMomentum()
+
+	if !car.DoorsOpen {
+		car.DoorsOpen = true
+		return // delay closing the doors by one tick
+	}
+
+	car.DoorsOpen = false
+	car.Direction = car.Momentum // car will start moving on the next tick
+}
+
+func (car *Car) shouldStop() bool {
+	// Is the car at one of the desired floors?
+	return car.DesiredFloors[car.DesiredFloors.Search(car.Floor)] == car.Floor
+}
+
+func (car *Car) checkMomentum() {
+	i := car.DesiredFloors.Search(car.Floor)
 }
